71d74337885ccf91878251af80ab81f80326417e
jesskfulwood <jesskfulwood@gmail.com>
2019-05-29 18:53:39 +0100

    tweak cmake parameters

--- build.rs
+++ build.rs
@@ -2,8 +2,11 @@ fn main() {
     let dst = cmake::Config::new("./nlopt-2.5.0")
         .define("BUILD_SHARED_LIBS", "OFF")
         .define("CMAKE_CXX_COMPILER", "c++") // Not used
+        // The default C++ flags mess things up - override them
+        .define("CMAKE_CXX_FLAGS", "")
         .build();
+    // Lib could be in either of two locations
     println!("cargo:rustc-link-search=native={}/lib", dst.display());
-    // println!("cargo:rustc-link-lib=nlopt");
-    // ^ apparently this is redundent
+    println!("cargo:rustc-link-search=native={}/lib64", dst.display());
+    println!("cargo:rustc-link-lib=static=nlopt");
 }
