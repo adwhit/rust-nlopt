aefdbd39c8dd49fb89eec8b6efeeafbb4722a303
relf <remi.lafage@onera.fr>
2021-01-19 11:19:43 +0100

    disable other nlopt bindings build

--- build.rs
+++ build.rs
@@ -1,9 +1,14 @@
 fn main() {
     let dst = cmake::Config::new("./nlopt-2.5.0")
         .define("BUILD_SHARED_LIBS", "OFF")
-        .define("CMAKE_CXX_COMPILER", "c++") // Not used
-        // The default C++ flags mess things up - override them
-        .define("CMAKE_CXX_FLAGS", "")
+        .define("CMAKE_CXX_COMPILER", "c++")
+        .define("NLOPT_CXX", "OFF")
+        .define("NLOPT_PYTHON", "OFF")
+        .define("NLOPT_OCTAVE", "OFF")
+        .define("NLOPT_MATLAB", "OFF")
+        .define("NLOPT_GUILE", "OFF")
+        .define("NLOPT_SWIG", "OFF")
+        .define("NLOPT_LINK_PYTHON", "OFF")
         .build();
     // Lib could be in either of two locations
     println!("cargo:rustc-link-search=native={}/lib", dst.display());
