ce690610b0ca570f5f267887771b94c164754f63
Stanislav Ravas <ravas@tind.sk>
2025-03-07 14:15:25 +0100

    fix(build.rs): don't override CMAKE_CXX_COMPILER, cross-compile would never work

--- build.rs
+++ build.rs
@@ -1,7 +1,6 @@
 fn main() {
     let dst = cmake::Config::new("./nlopt-2.9.1")
         .define("BUILD_SHARED_LIBS", "OFF")
-        .define("CMAKE_CXX_COMPILER", "c++")
         .define("NLOPT_CXX", "OFF")
         .define("NLOPT_PYTHON", "OFF")
         .define("NLOPT_OCTAVE", "OFF")
